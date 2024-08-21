60b84c4abe3f9e03748b39cb40d97be08cae43fa
Mandeep <Mandeepsinghjass@gmail.com>
2024-08-21 18:38:33 +0530

    Initialising project

--- App.tsx
+++ App.tsx
@@ -40,7 +40,7 @@ function Section({children, title}: SectionProps): React.JSX.Element {
             color: isDarkMode ? Colors.white : Colors.black,
           },
         ]}>
-        {title}
+        {"this is debugging"}
       </Text>
       <Text
         style={[
---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    project: {
+      android: {
+        packageName: 'com.awesome01',  // Replace with your actual package name
+      },
+    },
+    assets: [],
+    dependencies: {},
+  };
+  
