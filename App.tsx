aea5c97851c8f08b8397e0050c58f120f55e3193
Mandeep <Mandeepsinghjass@gmail.com>
2024-08-24 17:11:22 +0530

    Added fancy card with images and description.

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
 import FlatCards from './components/FlatCards'
 import ElevatedCards from './components/ElevatedCards'
+import FancyCard from './components/FancyCard'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <View>
       <FlatCards/>
       <ElevatedCards/>
+      <FancyCard/>
     </View>
     </ScrollView>
     </SafeAreaView>
